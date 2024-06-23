422546c504c7f78a9335d9312014fe76ce82faee
Lord-LightSpeed <adamkyte1@gmail.com>
2024-06-23 15:16:59 +0100

    starting to implement DynamicArray

---
+++ C/data structures/DynamicArray.h
@@ -0,0 +1,12 @@
+typedef struct
+{
+  void* items;
+  int length;
+  int capacity;
+  long unsigned int sizeofitem;
+} DynamicArray;
+
+void DynamicArrayAppend(DynamicArray* da, void* item)
+{
+
+};
