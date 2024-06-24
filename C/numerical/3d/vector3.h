1ec6bc25b22a1a2f7a66e302a765f38704620ec9
Lord-LightSpeed <adamkyte1@gmail.com>
2024-06-24 21:42:50 +0100

    added cross product

--- C/numerical/3d/vector3.h
+++ C/numerical/3d/vector3.h
@@ -6,3 +6,13 @@ typedef struct
   float Y;
   float Z;
 } Vector3;
+
+Vector3 Vec3Cross(Vector3 a, Vector3 b)
+{
+  return (Vector3)
+  {
+    .X = a.Y * b.Z - a.Z * b.Y,
+    .Y = a.X * b.Z - a.Z * b.X,
+    .Z = a.X * b.Z - a.Y * b.X
+  };
+};
