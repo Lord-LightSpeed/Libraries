3ad1c0b73c73ec80bc3fb1a31d6bc169b6530fb2
Lord-LightSpeed <adamkyte1@gmail.com>
2024-07-19 21:19:35 +0100

    began work on error system

---
+++ C/errors/personalerrors.h
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct
+{
+  char* err;
+  void* data;
+} Result;
+
+#ifdef LOG
+  void* unwrap(Result a, FILE* l)
+  {
+    if (a.err = NULL)
+    {
+      return a.data;
+    }
+    else
+    {
+      fprintf(l, "[ERROR]: %s", a.err);
+      fclose(l);
+      printf("[ERROR]: %s", a.err);
+      exit(-1);
+    };
+  };
+#else
+  void* unwrap(Result a)
+  {
+    if (a.err = NULL)
+    {
+      return a.data;
+    }
+    else
+    {
+      printf("[ERROR]: %s", a.err);
+      exit(-1);
+    };
+  };
+#endif
